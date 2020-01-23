72efa7e047c2314f525a191dd169449dc411444d
Alexander-Linkov <Alexander.Linkov@smartbear.com>
2020-01-23 16:20:53 +0300

    add assignment 5.1 (1 of 3)

--- module 5/app.js
+++ module 5/app.js
@@ -1,9 +1,20 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+    console.log("This always runs");
+    next();
+})
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    console.log('in product page middle');
+    res.send("<h1>The product page</h1>");
+});
+
+app.use('/', (req, res, next) => {
+    console.log("In root middleware!");
+    res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
