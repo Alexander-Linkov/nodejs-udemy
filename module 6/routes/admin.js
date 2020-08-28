d9c50fb24725f21a09712d8fc9a7486d06b95d20
Alexander-Linkov <Alexander.Linkov@smartbear.com>
2020-08-28 18:11:54 +0300

    lesson 78 completed

---
+++ module 6/routes/admin.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const express = require('express');
+const router = express.Router();
+
+const rootDir = require('../util/path');
+
+const products = [];
+
+
+// GET /admin/add-product
+router.get('/add-product', (req, res, next) => {
+    //console.log('in product page middle');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+});
+
+
+// POST /admin/add-product
+router.post('/add-product', (req, res, next) => {
+    products.push({title: req.body.title})
+    res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
